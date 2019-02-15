7eaf96498b547c765bfa692b134b7976cb44493f
huang <1323178720@qq.com>
2019-02-15 23:16:20 +0800

    学习Nuxt.js

---
+++ layouts/search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout_search">
+    <nuxt/>
+    <footer>这里是search模板</footer>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "search",
+  }
+</script>
+
+<style scoped>
+  .layout_search {
+    color: red;
+  }
+</style>
---
+++ pages/search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    Page in search
+    <ul>
+      <li v-for="(item,index) in $store.state.city.list" :key="index">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "search",
+    layout: "search",
+    data() {
+      return {
+        list: []
+      }
+    },
+    async asyncData() {
+      let {status, data: {list}} = await axios.get('http://localhost:3000/city/list');
+      if (status === 200) {
+        return {
+          list
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
